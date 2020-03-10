932d85418b6ab3b5a6d018e588a3366b01ec0c79
Matthew <matthew.robert.leroe@gmail.com>
2020-03-09 22:41:39 -0400

    JavaScript to Java...

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,5 +1,23 @@
 
 // if I could get an array out of a function, I could set it to ...
 
-document.querySelector('input').setAttribute('value', 'tomatoes');
-document.forms[0].submit()
+// 4th Stage: Integrate PizzaApp with
+
+// 3rd Stage
+// How do I replace the text input with the checkbox?
+
+
+// 2nd Stage
+// function adder(a, b) {
+//     return a + b;
+// }
+//
+// adder(2,5);
+//
+// document.querySelector('input').setAttribute('value', adder(2,5));
+// document.forms[0].submit()
+
+
+// 1st Stage
+// document.querySelector('input').setAttribute('value', 'tomatoes');
+// document.forms[0].submit()
