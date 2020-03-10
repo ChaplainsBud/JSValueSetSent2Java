4901b4461d682683c74109153999cc8b7aaf4b64
Matthew <matthew.robert.leroe@gmail.com>
2020-03-09 22:36:08 -0400

    I can set value directly; next, by a function, lastly, the event handler

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,6 +1,5 @@
 
 // if I could get an array out of a function, I could set it to ...
 
-document.getElementById('pizzaForm').value= "tomatoes";
-
+document.querySelector('input').setAttribute('value', 'tomatoes');
 document.forms[0].submit()
